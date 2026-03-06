774ebf0c1b7695c8286427bcdce13ddd0845b5b3
NandhiniChellapandi <nanthinic432@gmail.com>
2026-03-06 11:46:43 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Projects from './components/projects'
+import Home from './components/Home'
+
+export default function App() {
+  return (
+    <div>
+      <Home/>
+      <Projects/>
+      
+    </div>
+  )
+}
---
+++ src/components/Projects.jsx
@@ -0,0 +1,33 @@
+
+
+function Projects() {
+  return (
+    <>
+    <div className="container">
+
+      <div className="card">
+        <h2>Doctor Patient Portal</h2>
+        <p>
+          Doctor Patient Portal is my UG major project. This system is developed
+          to connect doctors and patients through an online platform where
+          patients can book appointments, view doctor availability and check
+          their medical details. It makes the appointment process simple and efficient.
+        </p>
+      </div>
+
+      <div className="card">
+        <h2>Pharmacy Management System</h2>
+        <p>
+          Pharmacy Management System is the project I developed during my UG.
+          This system is used to manage daily pharmacy activities such as
+          adding medicines, updating stock, generating bills and maintaining
+          customer records. It helps reduce manual work and improves efficiency.
+        </p>
+      </div>
+
+    </div>
+    </>
+  );
+}
+
+export default Projects
